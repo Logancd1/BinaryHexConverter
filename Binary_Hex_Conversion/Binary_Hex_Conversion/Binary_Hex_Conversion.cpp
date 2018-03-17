30bd476537207e2e9353381b3fb021f010d8f7d2
Logan Douglass <lcdouglass5352@pointloma.edu>
2018-03-16 22:35:17 -0700

    Created base files for C++ program. Added functional integer-to-binary and integer-to-hex conversion.

---
+++ Binary_Hex_Conversion/Binary_Hex_Conversion/Binary_Hex_Conversion.cpp
@@ -0,0 +1,45 @@
+// Binary_Hex_Conversion.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void printInteger(int n)
+{
+	cout << "Binary: " << bitset<32>(n) << endl;
+	//printf("%04X", n);
+	cout << "Hex: 0x" << hex << n << endl;
+}
+
+void printDouble(double d)
+{
+	cout << "Hex: 0x" << hex << d << endl;
+}
+
+int main()
+{
+	int integerInput = 0, choice = 0;
+	double doubleInput = 0;
+startOver:
+	cout << "Enter 1 if you're entering an integer and 2 if you're entering a double, 3 to exit: ";
+	cin >> choice;
+	switch (choice) {
+	case 1: 
+		cout << "Enter an integer (maximum 32 bits): ";
+		cin >> integerInput;
+		printInteger(integerInput);
+		goto startOver;
+	case 2: 
+		cout << "Enter a double: ";
+		cin >> doubleInput;
+		printDouble(doubleInput);
+		goto startOver;
+	case 3: break;
+	default: goto startOver;
+	}
+
+	return 0;
+}
